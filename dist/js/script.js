dbf046f88d9f7019f14388f991eeaf04377f3a72
nawcodes <nawcodes@gmail.com>
2023-03-26 05:39:58 +0700

    Clients

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+// Hamburger action
+const hamburgerEl = docuement.getElementById('hamburger');
+
+hamburgerEl.addEventListener('click', () => {
+    console.log('Hamburger clicked');
+    // hamburgerEl.classList.toggle('hamburger-active');
+});

1aa6de1eb16d70f1bdd09d8abf31f068f09acb37
nawcodes <nawcodes@gmail.com>
2023-04-01 14:16:45 +0700

    dark:bg-dark something wrong need to evaulate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -9,7 +10,6 @@ module.exports = {
     extend: {
       colors: {
         primary: '#14b8a6',
-        dark: '#0d7377',
         secondary: '#64748b',
       },
       screens: {

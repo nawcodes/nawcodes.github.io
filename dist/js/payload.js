d214adc518c20319edd3ef4991b420f0ea0ee200
nawcodes <nawcodes@gmail.com>
2023-05-02 15:10:24 +0700

    feat: load data from payload.js

---
+++ dist/js/payload.js
@@ -0,0 +1,43 @@
+window.projectPayload = [
+    {
+        name: 'project1',
+        description: 'description1',
+        image: 'image1',
+        link: 'link1'
+    },
+    {
+        name: 'project2',
+        description: 'description2',
+        image: 'image2',
+        link: 'link2'
+    },
+]
+
+
+window.clientsPayload = [
+    {
+        name: 'client1',
+        thumb: 'logo1',
+    },
+    {
+        name: 'client2',
+        thumb: 'logo2',
+    },
+]
+
+
+window.skillPayload = [
+    {
+        name: 'skill1',
+        image: 'image1',
+        score: 1
+    },
+    {
+        name: 'skill2',
+        image: 'image2',
+        score: 2
+    },
+]
+
+
+

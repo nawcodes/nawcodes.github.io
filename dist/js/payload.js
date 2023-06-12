37127cf0ac9ba3b269b1cc8bc903a312153940ff
nawcodes <nawcodes@gmail.com>
2023-06-12 15:10:34 +0700

    portfolio

--- dist/js/payload.js
+++ dist/js/payload.js
@@ -1,16 +1,41 @@
 window.projectPayload = [
     {
-        name: 'project1',
-        description: 'description1',
-        image: 'image1',
+        name: 'Larafurn - Laravel Ecommerce',
+        description: 'Larafurn adalah sebuah aplikasi ecommerce yang dibangun menggunakan framework laravel. Aplikasi ini berfokus pada penjualan furniture. Aplikasi ini memiliki fitur seperti keranjang belanja, checkout, dan lain-lain.',
+        image: 'larafurn.png',
         link: 'link1'
     },
     {
-        name: 'project2',
-        description: 'description2',
+        name: 'CI Shop - Codeigniter Ecommerce',
+        description: 'ci-shop adalah sebuah aplikasi ecommerce yang dibangun menggunakan framework codeigniter versi 3. Aplikasi ini berfokus pada penjualan online. B2B dan B2C. Aplikasi ini memiliki fitur seperti keranjang belanja, checkout, dan lain-lain.',
         image: 'image2',
         link: 'link2'
     },
+    {
+        name: 'Vue Cashier - Vue POS',
+        description: 'Vue Cashier adalah sebuah aplikasi point of sale yang dibangun menggunakan framework Vue, NodeJS, Express, MongoDB. Aplikasi ini berfokus pada penjualan offline. Aplikasi ini memiliki fitur seperti keranjang belanja, checkout, dan lain-lain.',
+        image: 'image3',
+        link: 'link3'
+    },
+    {
+        name: 'Village GIS - Laravel GIS',
+        description: 'Vilage GIS adalah sebuah aplikasi GIS yang dibangun menggunakan framework laravel. Aplikasi ini berfokus pemetaan desa. Aplikasi ini memiliki fitur seperti pemetaan desa, pemetaan jalan, pemetaan sungai, dan lain-lain.',
+        image: 'image4',
+        link: 'link4'
+    },
+    {
+        name: 'Vocational Correspondence Academy - CMS | Laravel',
+        description: 'Vocational Correspondence Academy adalah sebuah aplikasi CMS yang dibangun menggunakan framework Codeigniter V4. Aplikasi ini befokus pada alur pensuratan sesuai kejuruan yang ada di kampus, seperti surat keterangan, surat keterangan aktif kuliah, dan lain-lain.',
+    },
+    {
+        name: 'Vocational Correspondence Village - CMS | Codeigniter 4',
+        description: 'Vocational Correspondence Village adalah sebuah aplikasi CMS yang dibangun menggunakan framework Codeigniter V4. Aplikasi ini befokus pada alur pensuratan sesuai pemerintahan desa, seperti surat keterangan, surat keterangan pengajuan warga, dan lain-lain.',
+    },
+    {
+        name: 'Vechile Steam Management System - CMS | Codeigniter 4',
+        description: 'Vechile Steam Management System adalah sebuah aplikasi CMS yang dibangun menggunakan framework Codeigniter V4. Aplikasi ini befokus management data steam kendaraan, seperti data kendaraan, data sopir, data steam, dan lain-lain.',
+    }
+
 ]
 
 
